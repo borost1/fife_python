917cb5251e737260c3a2a454e78c02cadbd7abc3
Tamas Boros <boros.tamas.808@gmail.com>
2021-06-03 20:54:17 +0200

    adding all pngs +

---
+++ hatodik.py
@@ -0,0 +1,10 @@
+from random import randint
+
+resztvevok = [
+    'Márton Péter',
+    'Balogh Dorottya',
+    'Velev Milena',
+    'Kőszegi Bálint'
+]
+
+print(resztvevok[randint(0, len(resztvevok))])
